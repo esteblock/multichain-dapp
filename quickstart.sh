c73abb68bdca824e50b1747c4e6e7121ecfd844e
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-18 17:29:30 -0300

    using preview 8 quickstart

--- quickstart.sh
+++ quickstart.sh
@@ -6,10 +6,14 @@ case "$1" in
 standalone)
     echo "Using standalone network"
     ARGS="--standalone --enable-core-artificially-accelerate-time-for-testing"
+    STELLAR_NAME='stellar-standalone'
+    LOCAL_PORT='8000'
     ;;
 futurenet)
     echo "Using Futurenet network"
     ARGS="--futurenet"
+    STELLAR_NAME='stellar-futurenet'
+    LOCAL_PORT='8001'
     ;;
 *)
     echo "Usage: $0 standalone|futurenet"
@@ -17,14 +21,15 @@ futurenet)
     ;;
 esac
 
-shift
+
+
 
 echo "1. Creating docker soroban network"
 (docker network inspect soroban-network -f '{{.Id}}' 2>/dev/null) \
   || docker network create soroban-network
 
 
-echo "2. Running a soroban-precview docker container"
+echo "2. Running a soroban-preview docker container"
 previewVersion="8"
 
 echo "Searching for a previous soroban-preview docker container"
@@ -36,23 +41,40 @@ if [[ ${containerID} ]]; then
 else
     echo "No previous soroban-preview-${previewVersion} container was found"
 fi
+ 
 
 currentDir=$(pwd)
 docker run -dti \
   --volume ${currentDir}:/workspace \
   --name soroban-preview-${previewVersion} \
-  -p 8001:8000 \
   --ipc=host \
   --network soroban-network \
   esteblock/soroban-preview:${previewVersion}
 
+
+echo "2. Running a the stellar quickstart image with name=$STELLAR_NAME"
+
 # Run the stellar quickstart image
 docker run --rm -ti \
-  --name stellar \
+  --name $STELLAR_NAME \
   --network soroban-network \
-  -p 8000:8000 \
+  -p $LOCAL_PORT:8000 \
   stellar/quickstart:soroban-dev@sha256:a057ec6f06c6702c005693f8265ed1261e901b153a754e97cf18b0962257e872 \
   $ARGS \
   --enable-soroban-rpc \
   --protocol-version 20 \
   "$@" # Pass through args from the CLI
+
+
+
+
+
+  #--platform linux/amd64 \
+
+  # docker run --rm -it \
+  # -p 8002:8000 \
+  # --name stellar-futurenet \
+  # stellar/quickstart:soroban-dev@sha256:a057ec6f06c6702c005693f8265ed1261e901b153a754e97cf18b0962257e872 \
+  # --futurenet \
+  # --enable-soroban-rpc
+
