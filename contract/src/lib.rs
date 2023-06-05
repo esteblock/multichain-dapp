15c9ff7f62b3f97aaeff445f11be1dfe3c2d1775
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-06-05 20:29:43 -0300

    use symbol

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
-use soroban_sdk::{contractimpl, Env, Symbol, String};
+//use soroban_sdk::{contractimpl, Env, Symbol, String};
+use soroban_sdk::{contractimpl, Env, Symbol};
 
 const TITLE: Symbol = Symbol::short("TITLE");
 
@@ -9,14 +10,16 @@ pub struct TitleContract;
 impl TitleContract {
 
 
-    pub fn set_title(env: Env, title: String) {
+    //pub fn set_title(env: Env, title: String) {
+    pub fn set_title(env: Env, title: Symbol) {
                 env.storage().set(&TITLE, &title)
     }
 
 
-    pub fn read_title(env: Env) -> String {
+    //pub fn read_title(env: Env) -> String {
+    pub fn read_title(env: Env) -> Symbol {
         env.storage().get(&TITLE)
-            .unwrap_or(Ok(String::from_slice(&env, "Default Title")))
+            .unwrap_or(Ok(Symbol::short("TITLE")))
             .unwrap() 
     }
     
--- contract/src/test.rs
+++ contract/src/test.rs
@@ -1,7 +1,7 @@
 #![cfg(test)]
 
 use super::*;
-use soroban_sdk::{Env, String};
+use soroban_sdk::{Env, Symbol};
 
 
 #[test]
@@ -11,11 +11,11 @@ fn test() {
     let client = TitleContractClient::new(&env, &contract_id);
 
     let client_default_title = client.read_title(); 
-    assert_eq!(client_default_title, String::from_slice(&env, "Default Title"));
+    assert_eq!(client_default_title, Symbol::short("TITLE"));
 
-    client.set_title(&String::from_slice(&env, "New Title"));
+    client.set_title(&Symbol::new(&env, "new_title"));
     let client_new_title = client.read_title(); 
 
-    assert_eq!(client_new_title, String::from_slice(&env, "New Title"));
+    assert_eq!(client_new_title, Symbol::new(&env, "new_title"));
 
 }
