3b0a2e5febb99a278d6ac75694d6f91c1c3998f1
benjaminsalon <benjaminsalon.business@gmail.com>
2023-11-08 10:30:07 +0100

    Example of how to set a blockchain network connector for read only when there is no wallet connected yet

--- src/MySorobanReactProvider.tsx
+++ src/MySorobanReactProvider.tsx
@@ -3,6 +3,7 @@ import {SorobanReactProvider} from '@soroban-react/core';
 import {futurenet, sandbox, standalone} from '@soroban-react/chains';
 import {freighter} from '@soroban-react/freighter';
 import {ChainMetadata, Connector} from "@soroban-react/types";
+import { Server } from 'soroban-client';
       
 const chains: ChainMetadata[] = [sandbox, standalone, futurenet];
 const connectors: Connector[] = [freighter()]
@@ -13,6 +14,8 @@ const connectors: Connector[] = [freighter()]
       <SorobanReactProvider
         chains={chains}
         appName={"Example Stellar App"}
+        activeChain={standalone}
+        server={new Server('http://localhost:8000/soroban/rpc',{allowHttp:true})}
         connectors={connectors}>
           {children}
       </SorobanReactProvider>
